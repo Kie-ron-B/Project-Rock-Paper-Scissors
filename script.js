772410fcc6e19ad6d941ca5f277eff8194e76569
Kie-ron-B <237975216+Kie-ron-B@users.noreply.github.com>
2026-04-13 19:48:10 +0100

    Added steps 2 and 3. Computer and human input

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    const option = Math.floor(Math.random() * 3);
+    if (option === 0) {
+        return "Rock!"
+    }
+    else if (option === 1){
+        return "Paper!"    
+    }
+    else {
+        return "Scissors!"
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Rock, Paper or Scissors?");
+    return choice;
+}
+
+console.log ("Computer chose: ", getComputerChoice());
+console.log ("Human chose: ", getHumanChoice());
