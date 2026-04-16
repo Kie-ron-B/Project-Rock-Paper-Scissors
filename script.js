9699bcf3fe1df12fcaeaa3b681000592ffac7618
Kie-ron-B <237975216+Kie-ron-B@users.noreply.github.com>
2026-04-16 19:10:10 +0100

    Added the round score and the text displaying winner

--- script.js
+++ script.js
@@ -16,5 +16,27 @@ function getHumanChoice() {
     return choice;
 }
 
-console.log ("Computer chose: ", getComputerChoice());
-console.log ("Human chose: ", getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log('Draw, both you and the computer picked the same thing')
+    } else if (
+        humanChoice == 'paper' && computerChoice == 'rock',
+        humanChoice == 'rock' && computerChoice == 'scissors',
+        humanChoice == 'scissors' && computerChoice == 'paper'
+    ) {
+        console.log(`You win!! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else {
+        console.log(`You lose. ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(`Current Score - Human: ${humanScore} | Computer: ${computerScore}`);
