a1df5914328d5e6fa7a0e7c5181d29b56dd84ae4
Kie-ron-B <237975216+Kie-ron-B@users.noreply.github.com>
2026-04-16 22:27:09 +0100

    Added the 5 rounds and the overall winner/loser

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
 function getComputerChoice() {
     const option = Math.floor(Math.random() * 3);
     if (option === 0) {
-        return "Rock!"
+        return "rock"
     }
     else if (option === 1){
-        return "Paper!"    
+        return "paper"    
     }
     else {
-        return "Scissors!"
+        return "scissors"
     }
 }
 
 function getHumanChoice() {
     let choice = prompt("Rock, Paper or Scissors?");
-    return choice;
+    return choice.toLowerCase();
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
+    for (let i = 1; i <= 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
         console.log('Draw, both you and the computer picked the same thing')
-    } else if (
-        humanChoice == 'paper' && computerChoice == 'rock',
-        humanChoice == 'rock' && computerChoice == 'scissors',
-        humanChoice == 'scissors' && computerChoice == 'paper'
-    ) {
-        console.log(`You win!! ${humanChoice} beats ${computerChoice}`);
-        humanScore++;
-    } else {
-        console.log(`You lose. ${computerChoice} beats ${humanChoice}`);
-        computerScore++;
+        } else if (
+            humanChoice == 'paper' && computerChoice == 'rock' ||
+            humanChoice == 'rock' && computerChoice == 'scissors' ||
+            humanChoice == 'scissors' && computerChoice == 'paper'
+        ) {
+            console.log(`You win!! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        } else {
+            console.log(`You lose. ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
     }
+        console.log(`Current Score -> Human: ${humanScore} | Computer: ${computerScore}`);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+}
 
-playRound(humanSelection, computerSelection);
-console.log(`Current Score - Human: ${humanScore} | Computer: ${computerScore}`);
+if (humanScore > computerScore) {
+    console.log('Overall... You Win!')
+} else if (computerScore > humanScore) {
+    console.log('Overall... You Lose!')
+}   else {
+    console.log('Overall... Tie')
+}
+}
+playGame()
